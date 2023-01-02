c00066cfbd40e427cf7f8ddcc1ea13d9b3774050
Tony Kristiansen <121525720+tonykristiansen36@users.noreply.github.com>
2023-01-02 16:48:48 -0300

    Add files via upload

---
+++ code.php
@@ -0,0 +1,71 @@
+<?php
+session_start();
+require 'dbcon.php';
+
+if(isset($_POST['concluir_tarefa']))
+{
+    $tarefas_id = mysqli_real_escape_string($con, $_POST['concluir_tarefa']);
+
+    $query = "DELETE FROM tarefas WHERE id='$tarefas_id' ";
+    $query_run = mysqli_query($con, $query);
+
+    if($query_run)
+    {
+        $_SESSION['message'] = "Aluno excluido com sucesso";
+        header("Location: index.php");
+        exit(0);
+    }
+    else
+    {
+        $_SESSION['message'] = "Não foi possivel excluir o aluno";
+        header("Location: index.php");
+        exit(0);
+    }
+}
+
+if(isset($_POST['atualizar_tarefa']))
+{
+    $tarefas_id = mysqli_real_escape_string($con, $_POST['tarefas_id']);
+
+    $descricao = mysqli_real_escape_string($con, $_POST['descricao']);
+
+    $query = "UPDATE tarefas SET descricao='$descricao'";
+    $query_run = mysqli_query($con, $query);
+
+    if($query_run)
+    {
+        $_SESSION['message'] = "Tarefa atualizada com sucesso";
+        header("Location: index.php");
+        exit(0);
+    }
+    else
+    {
+        $_SESSION['message'] = "Tarefa não atualizada";
+        header("Location: index.php");
+        exit(0);
+    }
+
+}
+
+if(isset($_POST['registrar_tarefa']))
+{
+    $descricao = mysqli_real_escape_string($con, $_POST['descricao']);
+
+    $query = "INSERT INTO tarefas (descricao) VALUES ('$descricao')";
+
+    $query_run = mysqli_query($con, $query);
+    if($query_run)
+    {
+        $_SESSION['message'] = "Tarefa cadastrada com sucesso!";
+        header("Location: tarefa-create.php");
+        exit(0);
+    }
+    else
+    {
+        $_SESSION['message'] = "Tarefa não cadastrada";
+        header("Location: tarefa-create.php");
+        exit(0);
+    }
+}
+
+?>
---
+++ tarefa-create.php
@@ -0,0 +1,47 @@
+<?php
+session_start();
+?>
+
+<!doctype html>
+<html lang="pt-BR">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <title>Criar aluno</title>
+</head>
+<body>
+  
+    <div class="container mt-5">
+
+        <?php include('message.php'); ?>
+
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Adicionar tarefa
+                            <a href="index.php" class="btn btn-danger float-end">Voltar</a>
+                        </h4>
+                    </div>
+                    <div class="card-body">
+                        <form action="code.php" method="POST">
+
+                            <div class="mb-3">
+                                <label>Nome</label>
+                                <input type="text" name="descricao" class="form-control">
+                            </div>
+                            <div class="mb-3">
+                                <button type="submit" name="registrar_tarefa" class="btn btn-primary">Salvar tarefa</button>
+                            </div>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
