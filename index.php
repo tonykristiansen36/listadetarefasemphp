279040f2892d9f34ef41ff04312dd89c6f2920f9
Tony Kristiansen <121525720+tonykristiansen36@users.noreply.github.com>
2023-01-02 16:46:45 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,82 @@
+<?php
+    session_start();
+    require 'dbcon.php';
+?>
+<!doctype html>
+<html lang="pt-BR">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <title>Tarefas</title>
+</head>
+<body>
+  
+    <div class="container mt-4">
+
+        <?php include('message.php'); ?>
+
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Detalhes da tarefa
+                            <a href="tarefa-create.php" class="btn btn-primary float-end">Adicionar Tarefa</a>
+                        </h4>
+                    </div>
+                    <div class="card-body">
+
+                        <table class="table table-bordered table-striped">
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Descrição</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php 
+                                    $query = "SELECT * FROM tarefas";
+                                    $query_run = mysqli_query($con, $query);
+
+                                    if(mysqli_num_rows($query_run) > 0)
+                                    {
+                                        foreach($query_run as $tarefa)
+                                        {
+                                            ?>
+                                            <tr>
+                                                <td><?= $tarefa['id']; ?></td>
+                                                <td><?= $tarefa['descricao']; ?></td>
+                                                <td>
+                                                    <a href="tarefa-view.php?id=<?= $tarefa['id']; ?>" class="btn btn-info btn-sm">Visualizar</a>
+                                                    <a href="tarefa-edit.php?id=<?= $tarefa['id']; ?>" class="btn btn-success btn-sm">Editar</a>
+                                                    <form action="code.php" method="POST" class="d-inline">
+                                                        <button type="submit" name="concluir_tarefa" value="<?=$tarefa['id'];?>" class="btn btn-danger btn-sm">Deletar</button>
+                                                    </form>
+                                                </td>
+                                            </tr>
+                                            <?php
+                                        }
+                                    }
+                                    else
+                                    {
+                                        echo "<h5> Nenhuma tarefa cadastrada </h5>";
+                                    }
+                                ?>
+                                
+                            </tbody>
+                        </table>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
